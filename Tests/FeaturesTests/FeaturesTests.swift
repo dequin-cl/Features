86de3b1165014c9b653394935e0975edd13ea5b4
Iván GalazJeria <Ivan.galaz-jeria@auto1.com>
2021-09-02 18:12:39 +0200

    Feature names can be defined on an `Enumeration` as long as it conforms to `CustomStringConvertible`

--- Tests/FeaturesTests/FeaturesTests.swift
+++ Tests/FeaturesTests/FeaturesTests.swift
@@ -1,9 +1,15 @@
 import XCTest
 import Features
 
+protocol FeatureName: CustomStringConvertible {}
+
 enum Features {
-    static func isEnabled(_ name: String) -> Bool {
-        false
+    static func isEnabled(_ name: String, default: Bool = false) -> Bool {
+        `default`
+    }
+
+    static func isEnabled(_ name: FeatureName, default: Bool = false) -> Bool {
+        `default`
     }
 }
 
@@ -11,8 +17,40 @@ final class FeatureTests: XCTestCase {
 
     func test_nonExistingFeature_isDisabled() {
         let nonExistingFeature = "Any Feature"
+        
         let isEnabled = Features.isEnabled(nonExistingFeature)
         
-        XCTAssertFalse(isEnabled, "Expected \(nonExistingFeature) to be disabled. Got enabled")
+        XCTAssertFalse(isEnabled, "Expected \(nonExistingFeature) to be disabled. Got enabled.")
+    }
+    
+    func test_nonExistingFeature_isEnableWithTrueDefaultValue() {
+        let nonExistingFeature = "Any Feature"
+        
+        let isEnabled = Features.isEnabled(nonExistingFeature, default: true)
+        
+        XCTAssertTrue(isEnabled, "Expected \(nonExistingFeature) to be enabled. Got disabled.")
+
+    }
+
+    func test_nonExistingFeature_isDisableWithFalseDefaultValue() {
+        let nonExistingFeature = "Any Feature"
+        
+        let isEnabled = Features.isEnabled(nonExistingFeature, default: false)
+        
+        XCTAssertFalse(isEnabled, "Expected \(nonExistingFeature) to be disabled. Got enabled.")
+    }
+    
+    func test_featuresName_canBeAnEnumerationCase() {
+        
+        let isEnabled = Features.isEnabled(Features.Tests.test, default: true)
+        XCTAssertTrue(isEnabled, "Expected `\(Features.Tests.test)` to be enabled. Got disabled.")
+    }
+}
+
+extension Features {
+    enum Tests: String, FeatureName {
+        var description: String { rawValue }
+        
+        case test
     }
 }
